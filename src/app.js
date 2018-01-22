fc601102e3aee4ec7803cf233d5c8e839a4ecd82
unknown <basantkumar01@gmail.com>
2018-01-22 15:41:11 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const css = require('./app.scss');
 require('font-awesome/css/font-awesome.css');
-import './bootstrap-tabcollapse.js';
 $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
